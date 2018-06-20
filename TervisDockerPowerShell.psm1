d9ba4c937526847f7d23027b28a4cfef28851907
ChrisMagnuson <hourglasssand@gmail.com>
2018-06-20 17:03:07 -0400

    Add functions for running docker image

--- TervisDockerPowerShell.psm1
+++ TervisDockerPowerShell.psm1
@@ -5,3 +5,26 @@ function Remove-DockerContainerAllOff {
 function Get-DockerContainer {
     docker ps -a
 }
+
+function Get-DockerImage {
+    docker image ls
+}
+
+function Invoke-DockerContainer {
+    param (
+        $EnvironmentVariables,
+        $Name,
+        $ImageName
+    )
+    $Arguements = "run","--tty", "--interactive"
+
+    foreach ($Key in $EnvironmentVariables.Keys) {
+        $Arguements += @"
+--env $Key="$EnvironmentVariables.$Key"
+"@
+    }
+    
+    $Arguements += "--name", $Name, $ImageName
+
+    & docker $Arguements
+}
